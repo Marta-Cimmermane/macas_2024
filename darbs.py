692143eb4c980b3102adb40332f3c07dc7885e3c
Marta-Cimmermane <marta.cimmermane@grobinasskola.lv>
2024-11-21 14:32:45 +0200

    izlaboju kludas

--- darbs.py
+++ darbs.py
@@ -1,7 +1,9 @@
 # Uzraksti programmu, kurā  dators izvēlas 100 skaitļus robežās no 101 līdz 500. Izvēlētie skaitļi tiek izvadīti terminālī.
+izveletie_skaitli=[]
+import random
 
-import 
-
-for i in range():
-    random-skaitlis=random.randint(101,501,5)
-    print("random-skaitlis")
+for i in range(100):
+    random_skaitlis=random.randint(101,500,)
+    izveletie_skaitli.append(random_skaitlis)    
+for skaitli in izveletie_skaitli:
+    print(skaitli)
